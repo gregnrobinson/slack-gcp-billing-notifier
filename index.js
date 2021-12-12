a4ec3d94ad36f306a73fc2f4348ebc75ce4a0ef0
Greg Robinson <gregnrobinson@gmail.com>
2021-12-12 18:16:39 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const slack = require('slack');
 const {BigQuery} = require('@google-cloud/bigquery');
 
 const CHANNEL = 'billing';
-const DATASET = 'billing'
+const DATASET = 'phronesis_gcp_billing'
 const TABLE = 'budget';
 const PROJECT = process.env.GCP_PROJECT;
 const DATASET_LOCATION = 'northamerica-northeast1';
@@ -24,7 +24,7 @@ exports.notifySlack = async (data, context) => {
     if (!isFinite(threshold)){
         threshold = 0;
     }
-    //Create Rows to be placed in BigQuery
+    //save data 
     const rows = [{createdAt: createdAt, 
                     costAmount: pubsubData.costAmount, 
                     budgetAmount:pubsubData.budgetAmount, 
@@ -33,10 +33,9 @@ exports.notifySlack = async (data, context) => {
                     threshold: threshold}]
 
     
-    //Send to BigQuery Table
     await bigquery.dataset(DATASET).table(TABLE).insert(rows);
     
-    //Query the most recent data in BigQuery
+
     const query = `SELECT count(*) cnt
                     FROM \`${PROJECT}.${DATASET}.${TABLE}\`
                     WHERE createdAt >  TIMESTAMP( DATE(EXTRACT(YEAR FROM CURRENT_DATE()) , EXTRACT(MONTH FROM CURRENT_DATE()), 1)) 
@@ -53,16 +52,14 @@ exports.notifySlack = async (data, context) => {
     // Wait for the query to finish
     const [results] = await job.getQueryResults();
  
-    // If a notification was sent and the data remains the same for the second notification. Dont send another message.
     if (results.length > 0 && results[0].cnt > 1 ){
         return;
     }
     
-    //Emoji based on threshold
     const emoticon = threshold >= 90 ? ':fire:' : ':smiley:';
 
-    //Slack message
-    notification = `${emoticon} ${budgetName} \n\nThis is an automated notification to inform you that the billing account has exceeded ${threshold}% of the monthly budget of ${budgetAmount}.\n\nThe billing account has accrued ${costAmount} in costs so far for the month.`
+    //Test to be sent to Slack
+    notification = `${emoticon} ${budgetName} \n\nThis is an automated notification to inform you that the Phronesis billing account has exceeded ${threshold}% of the monthly budget of ${budgetAmount}.\n\nThe billing account has accrued ${costAmount} in costs so far for the month.`
 
     //Post to Slack channel with Bot Token
     await slack.chat.postMessage({
